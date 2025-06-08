20acdb667e74065a020a594795cdd81a2ef5efda
Sekh Aship <124449334+SekhAship@users.noreply.github.com>
2025-06-08 20:18:45 +0530

    Update firebaseConfig.jsx

--- Library_management/src/firebase/firebaseConfig.jsx
+++ Library_management/src/firebase/firebaseConfig.jsx
@@ -4,13 +4,7 @@ import { getAuth } from 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAVW37W3yPGzpdBLgdZieVVIMDbzUX3za0",
-  authDomain: "library-7e775.firebaseapp.com",
-  projectId: "library-7e775",
-  storageBucket: "library-7e775.firebasestorage.app",
-  messagingSenderId: "74644157354",
-  appId: "1:74644157354:web:caccbf58e8d11e79ec71d3",
-  measurementId: "G-V69HZXZ4WX"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
